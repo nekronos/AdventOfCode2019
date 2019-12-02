a0099aad677c70096e20feb0c74407d370d985c1
Vegard Strand Lende <lende.vegard@gmail.com>
2019-12-02 23:53:51 +0100

    Complete Day2 part 1

---
+++ Day2/Program.fs
@@ -0,0 +1,41 @@
+﻿open System
+open System.IO
+
+type IntCode = int[]
+
+exception InvalidInstruction of int
+
+let run (code: IntCode) =
+    let mutable pc = 0
+    while code.[pc] <> 99 do
+        let operator =
+            match code.[pc] with
+            | 1 -> (+)
+            | 2 -> (*)
+            | _ -> raise (InvalidInstruction(code.[pc]))
+        let left = code.[code.[pc + 1]]
+        let right = code.[code.[pc + 2]]
+        let result = operator left right
+        code.[code.[pc + 3]] <- result
+        pc <- pc + 4
+
+let loadIntCode filePath: IntCode =
+    (File.ReadAllText filePath).Split(',')
+    |> Array.map Int32.Parse
+   
+[<EntryPoint>]
+let main argv =
+
+    let intCode = loadIntCode "Input.txt"
+
+    // restore 1202 program state
+    intCode.[1] <- 12
+    intCode.[2] <- 2
+
+    run intCode |> ignore
+    
+    let result = intCode.[0]
+
+    printfn "Value at position 0: %A" result
+    
+    0
