c42496d4fb1f34151600f1bbf4c6d2084fb08d66
Vegard Strand Lende <lende.vegard@gmail.com>
2019-12-02 22:27:39 +0100

    Complete Day1 puzzle

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -1,8 +1,34 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open System.IO
 
-open System
+let fuelReq mass =
+    mass / 3 - 2 |> max 0
+
+let rec totalFuelReq mass =
+    match mass with
+    | 0 -> 0
+    | _ ->
+        let req = fuelReq mass
+        req + (totalFuelReq req)
+
+let calcFuelReq modules calcFn =
+    modules
+    |> Array.map calcFn
+    |> Array.reduce (+)
+
+let loadModules filePath =
+    File.ReadAllLines filePath
+    |> Array.map Int32.Parse
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+
+    let modules = loadModules "Input.txt"
+
+    let fuelReqSum = calcFuelReq modules fuelReq
+    printfn "Sum of fuel requirements: %A" fuelReqSum
+
+    let totalFuelReqSum = calcFuelReq modules totalFuelReq
+    printfn "Sum of total fuel requirements: %A" totalFuelReqSum
+
+    0
